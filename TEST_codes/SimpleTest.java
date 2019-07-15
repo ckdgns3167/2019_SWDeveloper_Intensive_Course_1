66cbe4767868506a50387c671314cf27c762c3f5
정창훈 <newckdgns@naver.com>
2019-07-15 17:04:03 +0900

    Java 교육 1일차 코드

---
+++ TEST_codes/SimpleTest.java
@@ -0,0 +1,15 @@
+public class SimpleTest{
+	public static void main(String[] args){
+		int loc;
+		loc = 8;
+		System.out.println(loc);
+		loc += 32;
+		System.out.println(loc);
+		loc += 16;
+		System.out.println(loc);
+		loc += 30;
+		System.out.println(loc);
+		loc += 24;
+		System.out.println(loc);
+	}
+}
